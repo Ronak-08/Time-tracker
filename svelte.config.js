19055c762a048d231210be0cbc23ff8d89d4d591
Ronak-08 <ronakameta65@gmail.com>
2026-07-04 12:22:38 +0530

    updated

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-	paths: { base: process.env.NODE_ENV === 'development' ? '' : '/Time-tracker' }}
+		adapter: adapter()
+	}
 };
 
 export default config;
